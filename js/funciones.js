ebd6fe8f75750a3003d5e6d66deef729e57d7386
MarinaGR <marinagr1984@gmail.com>
2014-12-30 10:52:58 +0100

    upload

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,42 @@
 function ajax_recover_data(type, container) {
-	var extern_url='http://vivirenavila.com/cebreros/api/v1/';
+	var extern_url='http://w3.cebreros.es/api/v1/';
+	
+	function successCallback(data) {
+		console.log(data);
+		//data = $.parseJSON(data);
+		var cadena="";
+		
+		$.each(data.Result.Items, function(index, d){            
+			cadena+="ID : "+d.ID+", "+
+					"Title : "+d.Title+", "+
+					"DatePublish : "+d.DatePublish+"<br>";
+		});
+		
+		$("#"+container).html(cadena);
+				
+	}
+	function jsonpCallback(data){
+		successCallback(data);
+	}
+	
+	$.ajax({
+	  url: extern_url+type,
+	  type: 'GET',
+	  dataType: 'json',
+	  crossDomain: true, 
+	  success: 'successCallback',
+	  error:function(jqXHR, textStatus, errorThrown){
+			alert('Error de conexión. No se han podido recuperar los datos. Por favor inténtalo de nuevo.');	
+			console.log(jqXHR);
+			console.log(textStatus+" "+errorThrown);
+	  },
+	  async:false,
+	});
+	
+}
+
+function ajax_recover_data_jsonp(type, container) {
+	var extern_url='http://w3.cebreros.es/api/v1/';
 	
 	function successCallback(data) {
 		console.log(data);
